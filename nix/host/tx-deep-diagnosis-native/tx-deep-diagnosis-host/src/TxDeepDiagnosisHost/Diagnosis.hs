7edfb43aa3486d8f51e142aa784a53931d90a437
paolino <paolo.veronelli@gmail.com>
2026-05-02 18:41:14 +0100

    style: fourmolu format the new tx-deep-diagnosis-host sources

--- nix/host/tx-deep-diagnosis-native/tx-deep-diagnosis-host/src/TxDeepDiagnosisHost/Diagnosis.hs
+++ nix/host/tx-deep-diagnosis-native/tx-deep-diagnosis-host/src/TxDeepDiagnosisHost/Diagnosis.hs
@@ -1,13 +1,13 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module TxDeepDiagnosisHost.Diagnosis
-    ( runIntent
-    , runValidate
-    , buildIntentRequest
-    , buildValidateRequest
-    , buildProducerTxsObject
-    , showInspectError
-    ) where
+module TxDeepDiagnosisHost.Diagnosis (
+    runIntent,
+    runValidate,
+    buildIntentRequest,
+    buildValidateRequest,
+    buildProducerTxsObject,
+    showInspectError,
+) where
 
 import qualified Conway.Inspector as Inspector
 import Data.Aeson (Value)
@@ -41,30 +41,40 @@ showInspectError (Inspector.UnknownLedgerOperation s) =
     "unknown_op: " <> Text.unpack s
 
 buildIntentRequest :: Text -> Value
-buildIntentRequest txHex = A.object
-    [ ("ledger_functional_layer", A.String "cardano-ledger-functional/v1")
-    , ("tx_cbor", A.String txHex)
-    , ("op", A.String "tx.intent")
-    , ("args", A.object [])
-    ]
+buildIntentRequest txHex =
+    A.object
+        [ ("ledger_functional_layer", A.String "cardano-ledger-functional/v1")
+        , ("tx_cbor", A.String txHex)
+        , ("op", A.String "tx.intent")
+        , ("args", A.object [])
+        ]
 
 buildValidateRequest :: Text -> Map Text Text -> Value
-buildValidateRequest txHex producerTxs = A.object
-    [ ("ledger_functional_layer", A.String "cardano-ledger-functional/v1")
-    , ("tx_cbor", A.String txHex)
-    , ("op", A.String "tx.validate")
-    , ("args", A.object
-        [ ("context", A.object
-            [ ("producer_txs", buildProducerTxsObject producerTxs) ]) ])
-    ]
+buildValidateRequest txHex producerTxs =
+    A.object
+        [ ("ledger_functional_layer", A.String "cardano-ledger-functional/v1")
+        , ("tx_cbor", A.String txHex)
+        , ("op", A.String "tx.validate")
+        ,
+            ( "args"
+            , A.object
+                [
+                    ( "context"
+                    , A.object
+                        [("producer_txs", buildProducerTxsObject producerTxs)]
+                    )
+                ]
+            )
+        ]
 
 buildProducerTxsObject :: Map Text Text -> Value
-buildProducerTxsObject m = A.object
-    [ ( Key.fromText txHash
-      , A.object
-          [ ("tx_cbor", A.String cborHex)
-          , ("source", A.String "blockfrost.txs.cbor")
-          ]
-      )
-    | (txHash, cborHex) <- Map.toList m
-    ]
+buildProducerTxsObject m =
+    A.object
+        [ ( Key.fromText txHash
+          , A.object
+                [ ("tx_cbor", A.String cborHex)
+                , ("source", A.String "blockfrost.txs.cbor")
+                ]
+          )
+        | (txHash, cborHex) <- Map.toList m
+        ]
