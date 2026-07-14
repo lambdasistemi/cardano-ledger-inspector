9df8dffb46252f3d6ed542a6753c5eb9373185ae
paolino <paolo.veronelli@gmail.com>
2026-07-14 09:25:54 +0100

    chore: extend gate.sh for protocol registry flake output
    Tasks: T000

--- gate.sh
+++ gate.sh
@@ -5,3 +5,4 @@ git diff --check
 just ui-check
 just build-ui
 nix develop --quiet -c sh -c 'cd docs/inspector && ln -sfn $(dirname $(dirname $(readlink -f $(command -v playwright))))/lib/node_modules node_modules && TX_INSPECTOR_SITE_DIR=../../result playwright test tests/tx-identify.spec.mjs --grep "generic hex-suffix credential resolution" --reporter=list'
+nix build --print-out-paths .#protocol-registry .#checks.x86_64-linux.protocol-registry-drift-check
